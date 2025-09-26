8c882e5b0057a1ab93fd11d21aa109a86a923824
socrastein <31764357+socrastein@users.noreply.github.com>
2025-09-26 16:03:59 -0600

    Add header with player name and score, implement highlighting on player's turn

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { state } from '@/scripts/gameState'
+
 import Tile from './Tile.vue'
 
 const gameTokens = ref(state.gameTokens)
-const players = ref(state.players)
-const currentPlayer = ref(state.players[state.currentPlayerIndex])
+const currentPlayer = computed(() => state.players[state.currentPlayerIndex])
 </script>
 
 <template>
   <div class="mainContainer">
     <div class="gameHeader">
-      <h1 v-for="player in players">{{ player.name }}</h1>
+      <h1
+        v-for="player in state.players"
+        :key="player.name"
+        class="playerName"
+        :class="{ highlight: player === currentPlayer }"
+      >
+        {{ player.name }} | {{ player.tokens.length }}
+      </h1>
     </div>
     <div class="tokensContainer">
-
       <Tile v-for="token in gameTokens" :token="token" />
     </div>
   </div>
@@ -35,8 +41,17 @@ const currentPlayer = ref(state.players[state.currentPlayerIndex])
   width: 90%;
 }
 
+.playerName {
+}
+
+.highlight {
+  border: solid 1px red;
+}
+
 .tokensContainer {
   display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
   gap: 2rem;
 }
 </style>
