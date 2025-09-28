3beca76247a59fd8f808c4a4b86a7e889d75ce3b
socrastein <31764357+socrastein@users.noreply.github.com>
2025-09-28 15:05:49 -0600

    Add end game logic, extract several functions out of state object

--- src/scripts/gameState.ts
+++ src/scripts/gameState.ts
@@ -5,8 +5,14 @@ import { Player } from './classPlayer'
 import { getTokens } from './tokenArray'
 
 interface State {
+  showMenu: boolean
+  showGameEnd: boolean
+  currentlyEvaluating: boolean
+
   players: Player[]
   currentPlayerIndex: number
+  getCurrentPlayer: Function
+  winningPlayer: Player | undefined
 
   gameTokens: Token[]
   matchedThisTurn: Token[]
@@ -14,18 +20,23 @@ interface State {
   firstFlipped: Token | undefined
   secondFlipped: Token | undefined
 
-  currentlyEvaluating: boolean
-
   startNewGame: Function
-  nextPlayerTurn: Function
   evaluateFlip: Function
-
-  logTokens: Function
 }
 
 export const state = reactive<State>({
+  showMenu: true,
+  showGameEnd: true,
+  currentlyEvaluating: false,
+
   players: [],
   currentPlayerIndex: 0,
+
+  getCurrentPlayer(): Player {
+    return this.players[this.currentPlayerIndex]
+  },
+
+  winningPlayer: undefined,
 
   gameTokens: [],
   matchedThisTurn: [],
@@ -33,30 +44,17 @@ export const state = reactive<State>({
   firstFlipped: undefined,
   secondFlipped: undefined,
 
-  currentlyEvaluating: false,
-
-  startNewGame() {
+  startNewGame(numberOfTokens = 27) {
+    this.gameTokens = getTokens(numberOfTokens)
+    resetVariables()
     setNewPlayers()
-    resetFlipped()
-    this.gameTokens = getTokens(27)
-  },
-
-  nextPlayerTurn() {
-    const lastPlayerIndex = this.players.length - 1
-    if (this.currentPlayerIndex === lastPlayerIndex) {
-      this.currentPlayerIndex = 0
-    } else {
-      this.currentPlayerIndex++
-    }
-    console.log(`${this.players[this.currentPlayerIndex].name}'s turn`)
-    resetFlipped()
-    flipAllTokens()
   },
 
   evaluateFlip(token: Token) {
     // Don't allow more clicking between timeouts
     if (this.currentlyEvaluating) return
 
+    // Player has clicked the first token
     if (this.firstFlipped === undefined) {
       this.firstFlipped = token
       token.revealed = true
@@ -68,31 +66,17 @@ export const state = reactive<State>({
       return
     }
 
+    // Player has clicked a second token
     this.secondFlipped = token
     token.revealed = true
 
-    // Player clicks matching token
+    // Second token matches the first
     if (this.firstFlipped.name === this.secondFlipped.name) {
-      this.matchedThisTurn.push(this.firstFlipped, this.secondFlipped)
-      resetFlipped()
-      return
+      handleMatch()
     }
 
-    // Player clicks non-matching token
-    this.currentlyEvaluating = true
-    setTimeout(() => {
-      this.nextPlayerTurn()
-      this.currentlyEvaluating = false
-    }, 1000)
-  },
-
-  logTokens() {
-    const length = this.gameTokens.length
-    console.log(`Logging ${length} state.gameTokens: `)
-
-    for (let i = 0; i < length; i++) {
-      console.log(this.gameTokens[i].name)
-    }
+    // Second token does not match the first
+    handleMiss()
   },
 })
 
@@ -100,13 +84,64 @@ function setNewPlayers(name1 = 'Player 1', name2 = 'Player 2') {
   const player1 = new Player(name1)
   const player2 = new Player(name2)
 
+  //TOKENS FOR TESTING
+  player1.tokens = [...state.gameTokens.slice(0, 10)]
+  player2.tokens = [...state.gameTokens.slice(10)]
+  state.winningPlayer = player2
+
   state.players = [player1, player2]
   state.currentPlayerIndex = 0
 }
 
-function resetFlipped() {
+function nextPlayerTurn() {
+  const lastPlayerIndex = state.players.length - 1
+  if (state.currentPlayerIndex === lastPlayerIndex) {
+    state.currentPlayerIndex = 0
+  } else {
+    state.currentPlayerIndex++
+  }
+  console.log(`${state.players[state.currentPlayerIndex].name}'s turn`)
+  resetVariables()
+  flipAllTokens()
+}
+
+function handleMatch() {
+  if (!(state.firstFlipped && state.secondFlipped)) return
+
+  state.matchedThisTurn.push(state.firstFlipped, state.secondFlipped)
+  if (hasGameBeenWon()) {
+    endGame()
+    return
+  }
+  resetVariables()
+  return
+}
+
+function handleMiss() {
+  state.currentlyEvaluating = true
+  setTimeout(() => {
+    const player = state.getCurrentPlayer()
+    const tokensTaken: String[] = []
+
+    state.matchedThisTurn.forEach((token) => {
+      token.matched = true
+      if (tokensTaken.includes(token.name)) {
+        return
+      }
+      tokensTaken.push(token.name)
+      player.takeTokens(token.name)
+    })
+    nextPlayerTurn()
+    state.currentlyEvaluating = false
+  }, 1500)
+}
+
+function resetVariables() {
   state.firstFlipped = undefined
   state.secondFlipped = undefined
+  state.showMenu = false
+  state.showGameEnd = false
+  state.winningPlayer = undefined
 }
 
 function flipAllTokens() {
@@ -114,4 +149,26 @@ function flipAllTokens() {
   for (let i = 0; i < length; i++) {
     state.gameTokens[i].revealed = false
   }
+}
+
+function hasGameBeenWon(): boolean {
+  state.gameTokens.forEach((token) => {
+    if ((token.matched = false)) {
+      return false
+    }
+    // All tokens have been matched
+  })
+  return true
+}
+
+function endGame() {
+  state.showGameEnd = true
+
+  let maxScore = 0
+  state.players.forEach((player) => {
+    if (player.tokens.length > maxScore) {
+      maxScore = player.tokens.length
+      state.winningPlayer = player
+    }
+  })
 }
